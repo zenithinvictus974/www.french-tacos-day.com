d1a87bc558c3ce2cd032776e6b3cc36a707371f9
zenithinvictus974 <zenith.invictus974@gmail.com>
2025-09-24 21:50:05 +0400

    Initial commit from your app

--- src/sections/ParticipationSection/components/PrizesSection.tsx
+++ src/sections/ParticipationSection/components/PrizesSection.tsx
@@ -2,7 +2,7 @@ export const PrizesSection = () => {
   return (
     <div className="box-border caret-transparent w-full flex justify-center py-8">
       <img
-        src="https://c.animaapp.com/mfxmfzp8WHj4Hi/img/lots-a-gagner.png"
+        src="https://c.animaapp.com/mfxmfzp8WHj4Hi/img/lots-a-gagner_1.png"
         alt="Les Lots à Gagner"
         className="box-border caret-transparent max-w-full w-[90%] mx-auto md:w-auto"
       />
